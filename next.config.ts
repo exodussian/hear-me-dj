3fb8ed69e3b2d45a842d6e74dfbf508422e89b5c
exodussian <hakankaragol@gmail.com>
2025-03-06 22:37:25 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXTAUTH_URL: "http://localhost:3000",
-    NEXTAUTH_SECRET: "rasgele-guvenli-bir-string",
-    GOOGLE_CLIENT_ID: "84271347914-c62816s9vjpp07r0ui80tavp070e3c0v.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
+
   },
 }
 
