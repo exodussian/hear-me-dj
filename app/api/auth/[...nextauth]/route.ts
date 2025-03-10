776c8ab589d36e2b9e7a9a32d6fd0d8f5a3069f9
Hakan Karagöl <hakankaragol@gmail.com>
2025-03-10 15:22:47 +0100

    guzall

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,19 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from '../../../../src/lib/prisma';
+import type { Session, User, DefaultSession } from "next-auth";
+import type { JWT } from "next-auth/jwt";
+import type { AdapterUser } from "next-auth/adapters";
+
+// DefaultSession'ı genişlet
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id?: string;
+      settings?: any;
+    } & DefaultSession["user"]
+  }
+}
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -13,7 +26,7 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async session({ session, token, user }: { session: any; token: any; user: any }) {
+    async session({ session, token, user }: { session: Session; token: JWT; user: AdapterUser }) {
       if (session.user) {
         // id'yi token veya user'dan al
         session.user.id = token?.sub || user?.id;
@@ -28,7 +41,6 @@ export const authOptions = {
       }
       return session;
     },
-    // ...
   
     async redirect({ url, baseUrl }: { url: string; baseUrl: string }) {
       // Yönlendirme kodu
